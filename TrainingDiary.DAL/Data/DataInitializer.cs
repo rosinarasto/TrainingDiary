f3db87773a553eeeef7b42a23b4ef0172f95756f
Rastislav Rosina <xrosina@fi.muni.cz>
2024-11-20 15:34:11 +0100

    feat: added DBContext and skeleton for DataInitializer

---
+++ TrainingDiary.DAL/Data/DataInitializer.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TrainingDiary.DAL.Models;
+
+namespace TrainingDiary.DAL.Data
+{
+    public static class DataInitializer
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+            var users = PrepareUserModels();
+            var accounts = PrepareAccountModels();
+            var trainingRecords = PrepairTrainingRecordModels();
+            var recordFields = PrepairRecordFieldModels();
+
+            modelBuilder.Entity<User>()
+                .HasData(users);
+
+            modelBuilder.Entity<Account>()
+                .HasData(accounts);
+
+            modelBuilder.Entity<TrainingRecord>()
+                .HasData(trainingRecords);
+
+            modelBuilder.Entity<RecordField>()
+                .HasData(recordFields);
+        }
+
+        private static List<RecordField> PrepairRecordFieldModels()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static List<TrainingRecord> PrepairTrainingRecordModels()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static List<Account> PrepareAccountModels()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static List<User> PrepareUserModels()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TrainingDiary.DAL/Data/TrainingDiaryDBContext.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TrainingDiary.DAL.Models;
+
+namespace TrainingDiary.DAL.Data
+{
+    public class TrainingDiaryDBContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<TrainingRecord> TrainingRecords { get; set; }
+        public DbSet<RecordField> RecordFields { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Account>()
+                .HasMany(a => a.RestrictedUsers)
+                .WithMany(u => u.RestrictedAccounts)
+                .UsingEntity<Dictionary<string, object>>(
+                    "AccountStudent",
+                    j => j.HasOne<User>().WithMany().HasForeignKey("UserID"),
+                    j => j.HasOne<Account>().WithMany().HasForeignKey("AccountID"));
+
+            modelBuilder.Entity<User>()
+                .HasOne(u => u.OwnedAccount)
+                .WithOne(a => a.Owner)
+                .HasForeignKey<Account>(a => a.OwnerID)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<TrainingRecord>()
+                .HasOne(d => d.Owner)
+                .WithMany(o => o.TrainingRecords)
+                .HasForeignKey(d => d.OwnerId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<RecordField>()
+                .HasOne(f => f.TrainingRecord)
+                .WithMany(r => r.RecordFields)
+                .HasForeignKey(f => f.RecordID)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Seed();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
